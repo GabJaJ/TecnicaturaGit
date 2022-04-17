5c8615fec48feff3baa6e47777199c28bed444f1
Gabriel Juhasz <gabrieljaj@gmail.com>
2022-04-17 20:32:47 -0300

    Actualizado Variables Python

---
+++ Leccion1/main.py
@@ -0,0 +1,16 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos!!"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+miVariable = "Chau a todos!!"
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(x))
+print(id(y))
+print(id(z))
+# las literales se escriben x352, y096
